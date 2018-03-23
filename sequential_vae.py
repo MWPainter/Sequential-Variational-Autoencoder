005247d3eacd7bf16f398afa980f7893c61e1bf2
MWPainter <michaelpainter.1994@gmail.com>
2018-03-22 17:17:04 -0700

    Added some models that I want to run

--- sequential_vae.py
+++ sequential_vae.py
@@ -180,7 +180,7 @@ class SequentialVAE(Network):
         # python main.py --dataset=mnist --plot_reconstruction --netname=sequential_vae_mnist_inhomog
         elif self.name == "sequential_vae_mnist_inhomog":
             self.vlae_levels = 3
-            self.vlae_latent_dims = [8, 8, 8]
+            self.vlae_latent_dims = [2, 2, 2]
             self.latent_dim = np.sum(self.vlae_latent_dims)
             self.image_sizes = [32, 16, 8, 4] 
             self.filter_sizes = [self.data_dims[-1], 64, 128, 192, 256]
@@ -190,7 +190,7 @@ class SequentialVAE(Network):
 
         elif self.name == "sequential_vae_mnist_homog":
             self.vlae_levels = 3
-            self.vlae_latent_dims = [8, 8, 8]
+            self.vlae_latent_dims = [2, 2, 2]
             self.latent_dim = np.sum(self.vlae_latent_dims)
             self.image_sizes = [32, 16, 8, 4] 
             self.filter_sizes = [self.data_dims[-1], 64, 128, 192, 256]
@@ -201,7 +201,7 @@ class SequentialVAE(Network):
 
         elif self.name == "sequential_vae_mnist_share_all":
             self.vlae_levels = 3
-            self.vlae_latent_dims = [8, 8, 8]
+            self.vlae_latent_dims = [2, 2, 2]
             self.latent_dim = np.sum(self.vlae_latent_dims)
             self.image_sizes = [32, 16, 8, 4] 
             self.filter_sizes = [self.data_dims[-1], 64, 128, 192, 256]
@@ -209,6 +209,28 @@ class SequentialVAE(Network):
             self.generator = self.generator_ladder
             self.inference = self.inference_ladder
             self.share_generative_params = True
+            self.share_recognition_params = True
+
+        elif self.name == "sequential_vae_mnist_share_encoders":
+            self.vlae_levels = 3
+            self.vlae_latent_dims = [2, 2, 2]
+            self.latent_dim = np.sum(self.vlae_latent_dims)
+            self.image_sizes = [32, 16, 8, 4] 
+            self.filter_sizes = [self.data_dims[-1], 64, 128, 192, 256]
+            self.steps = 5
+            self.generator = self.generator_ladder
+            self.inference = self.inference_ladder
+            self.share_encoder_params = True
+
+        elif self.name == "sequential_vae_mnist_share_inference":
+            self.vlae_levels = 3
+            self.vlae_latent_dims = [2, 2, 2]
+            self.latent_dim = np.sum(self.vlae_latent_dims)
+            self.image_sizes = [32, 16, 8, 4] 
+            self.filter_sizes = [self.data_dims[-1], 64, 128, 192, 256]
+            self.steps = 5
+            self.generator = self.generator_ladder
+            self.inference = self.inference_ladder
             self.share_recognition_params = True
 
         else:
