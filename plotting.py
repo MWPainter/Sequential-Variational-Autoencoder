cef4867fe8269fa58c18790254d2f1a4ae423479
MWPainter <michaelpainter.1994@gmail.com>
2018-04-02 05:38:12 -0700

    Skeleton files for eventual plotting of data

---
+++ plotting.py
@@ -0,0 +1,2 @@
+# Todo: Take the output from tensorboard_extract.py, and produce nice, seaborn graphs with it
+# Todo: Put any/all other plotting code here
---
+++ tensorboard_extract.py
@@ -0,0 +1,2 @@
+# Todo: Write helper functions to extract data from tensorboard summaries that are written
+# Todo: output it in something easy for us to use, like a csv
